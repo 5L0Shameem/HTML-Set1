fe5f96409a397ee707279b59f0e031548c88f6d3
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-15 08:21:03 -0700

    Add files via upload

---
+++ Week 4/Day 4/SimpleCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+class SimpleCalculatorProgram
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        Console.Write("Enter First Number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int addition = calc.Add(num1, num2);
+        int subtraction = calc.Subtract(num1, num2);
+
+        Console.WriteLine("Addition = " + addition);
+        Console.WriteLine("Subtraction = " + subtraction);
+    }
+}
