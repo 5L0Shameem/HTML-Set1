00ae90b37da2acf4fbeb3093eb942213f6affcfc
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-15 08:02:00 -0700

    Add files via upload

---
+++ Week 4/Day 3/Q2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Q2
+{
+    static void Main()
+    {
+        Console.Write("Enter First Number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Operator (+, -, *, /): ");
+        char op = Console.ReadLine()[0];
+
+        double result = 0;
+        bool valid = true;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Error: Division by zero");
+                    valid = false;
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid Operator");
+                valid = false;
+                break;
+        }
+
+        if (valid)
+            Console.WriteLine("Result: " + result);
+    }
+}
