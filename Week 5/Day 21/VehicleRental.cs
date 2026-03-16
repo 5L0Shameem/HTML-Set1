a4f4b4fc0dbdcd0d553f65fea0c513776b961df2
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-16 04:57:05 -0700

    Add files via upload

---
+++ Week 5/Day 21/VehicleRental.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Vehicle
+{
+    private string brand;
+    private double rentalRatePerDay;
+
+    public string Brand
+    {
+        get { return brand; }
+        set { brand = value; }
+    }
+
+    public double RentalRatePerDay
+    {
+        get { return rentalRatePerDay; }
+        set
+        {
+            if (value >= 0)
+                rentalRatePerDay = value;
+            else
+                Console.WriteLine("Invalid rental rate");
+        }
+    }
+
+    public virtual double CalculateRental(int days)
+    {
+        return RentalRatePerDay * days;
+    }
+}
+
+class Car : Vehicle
+{
+    public override double CalculateRental(int days)
+    {
+        if (days <= 0)
+        {
+            Console.WriteLine("Invalid rental days");
+            return 0;
+        }
+
+        double total = RentalRatePerDay * days;
+        return total + 500;
+    }
+}
+
+class Bike : Vehicle
+{
+    public override double CalculateRental(int days)
+    {
+        if (days <= 0)
+        {
+            Console.WriteLine("Invalid rental days");
+            return 0;
+        }
+
+        double total = RentalRatePerDay * days;
+        return total - (total * 0.05);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle car = new Car();
+        car.Brand = "Toyota";
+        car.RentalRatePerDay = 2000;
+
+        int days = 3;
+
+        Console.WriteLine("Total Rental = " + car.CalculateRental(days));
+    }
+}
