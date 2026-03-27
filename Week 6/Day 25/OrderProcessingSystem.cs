a8065c207f775cca070286fbaa341eec3e087842
5L0Shameem <133978603+5L0Shameem@users.noreply.github.com>
2026-03-27 08:44:16 -0700

    Add files via upload

---
+++ Week 6/Day 25/OrderProcessingSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class OrderProcessingSystem
+{
+    static async Task VerifyPaymentAsync()
+    {
+        Console.WriteLine("Payment Verification Started");
+        await Task.Delay(2000);
+        Console.WriteLine("Payment Verified");
+    }
+
+    static async Task CheckInventoryAsync()
+    {
+        Console.WriteLine("Inventory Check Started");
+        await Task.Delay(2000);
+        Console.WriteLine("Inventory Available");
+    }
+
+    static async Task ConfirmOrderAsync()
+    {
+        Console.WriteLine("Order Confirmation Started");
+        await Task.Delay(2000);
+        Console.WriteLine("Order Confirmed");
+    }
+
+    static async Task Main()
+    {
+        Console.WriteLine("Order Processing Started\n");
+
+        await VerifyPaymentAsync();
+        await CheckInventoryAsync();
+        await ConfirmOrderAsync();
+
+        Console.WriteLine("\nOrder Processing Completed");
+    }
+}
